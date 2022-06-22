2e33ed2f15c7473ea35497edb0a706ca780b89ef
Saifullah Sammo <saifullahsammo@gmail.com>
2022-06-23 01:55:36 +0600

    header fixed

---
+++ .history/bislite/assets/js/script_20220623015240.js
@@ -0,0 +1,25 @@
+(function ($) {
+  //dom ready start
+  $(document).ready(function () {
+    //testimonial carousel
+    $(".slider").owlCarousel({
+      items: 4,
+      autoplay: true,
+      loop: true,
+      smartSpeed: 500,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        769: {
+          items: 3,
+        },
+        992: {
+          items: 4,
+        },
+      },
+    });
+  });
+  //dom ready end
+})(jQuery);
